6d4920d9bd8a1edff87f2f49c6e62b9fa0c70800
ekhrapan <yauheni.khrapan@gmail.com>
2020-03-30 17:14:41 +0300

    code

---
+++ src/academy/belhard/Main.java
@@ -0,0 +1,48 @@
+package academy.belhard;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = new int[3];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+
+        log("[Корректное поведение]");
+        try {
+            int lastElement = array[array.length - 1];
+            log("Последний элемент последовательности = " + lastElement);
+        } catch (IndexOutOfBoundsException e) {
+            log("ОШИБКА: Выход за пределы массива");
+        }
+
+        emptyLines(2);
+
+        log("[Обработка исключения]");
+        try {
+            // Конфликта имен не происходит, т.к. каждый try имеет свою облать видимости
+            int lastElement = array[array.length];
+            log("Последний элемент последовательности = " + lastElement);
+        } catch (IndexOutOfBoundsException e) {
+            log("ОШИБКА: Выход за пределы массива");
+        }
+
+        emptyLines(2);
+
+        log("[Без обработки исключения]");
+        int lastElement = array[3];
+        log("Последний элемент последовательности = " + lastElement);
+
+        log("Программа завершена"); // Не будет выведена !!!
+    }
+
+    public static void log(String message) {
+        System.out.println(message);
+    }
+
+    public static void emptyLines(int emptyLinesCount) {
+        for (int i = 0; i < emptyLinesCount; i++) {
+            System.out.println();
+        }
+    }
+}
